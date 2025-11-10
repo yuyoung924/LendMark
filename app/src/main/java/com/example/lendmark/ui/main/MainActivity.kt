d8cf74803a23c9780e0726fe54a9e2098852044a
yuyoung924 <hyyy8810@gmail.com>
2025-11-10 15:32:25 +0900

    Feat : building & rooms DB initial sample upload

--- app/src/main/java/com/example/lendmark/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/lendmark/ui/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.lendmark.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.lendmark.R
@@ -9,11 +10,14 @@ import com.example.lendmark.ui.my.MyPageFragment
 import com.example.lendmark.ui.reservation.BuildingListFragment
 import com.example.lendmark.ui.reservation.ReservationMapFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        uploadInitialData()
         setContentView(R.layout.activity_main)
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
@@ -45,6 +49,7 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
+        val db = FirebaseFirestore.getInstance()
     }
 
     private fun replaceFragment(fragment: Fragment) {
@@ -52,4 +57,37 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.main_container, fragment)
             .commit()
     }
+
+    fun uploadInitialData() {
+        val db = FirebaseFirestore.getInstance()
+
+        // 1. Buildings & Rooms
+        val dasan = hashMapOf(
+            "name" to "Dasan Hall",
+            "code" to -1,
+            "naverMapLat" to 37.631,
+            "naverMapLng" to 127.078
+        )
+        db.collection("buildings").document("dasan").set(dasan)
+
+        val rooms = listOf(
+            mapOf("name" to "101", "capacity" to 40,
+                "facilities" to listOf("projector", "whiteboard", "power-outlets")),
+            mapOf("name" to "107", "capacity" to 120,
+                "facilities" to listOf("projector", "whiteboard", "mic", "hdmi", "aircon"))
+        )
+        for (room in rooms) {
+            db.collection("buildings").document("dasan")
+                .collection("rooms").document("dasan-${room["name"]}")
+                .set(room)
+        }
+
+
+
+
+    }
+
+
+
+
 }
